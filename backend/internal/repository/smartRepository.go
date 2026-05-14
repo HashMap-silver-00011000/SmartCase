4d67cc9d81af7e3298e3a2f2a5bc3105352322d8
SilvaFresc <silver.bin.00011000@gmail.com>
2026-05-14 16:47:50 -0500

    Update smartRepository.go

--- backend/internal/repository/smartRepository.go
+++ backend/internal/repository/smartRepository.go
@@ -1,1 +1,33 @@
+package repository
 
+import (
+
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	"backend/internal/models"
+)
+
+
+type SmartRepository struct{
+	db *sqlx.DB
+}
+
+func NewSmartCaseRepository(db *sqlx.DB) *SmartRepository{
+
+	if db == nil {
+		panic("No puedes crear un caja sin una base de datos")
+	}
+	return &SmartRepository{db:db}
+}
+
+func (r *SmartRepository) CrearSede(Sede *models.SmartCase) error {
+
+	_ ,err := r.db.NamedExec(`INSERT INTO SmartCase 
+    						(id_caja, estado_solenoide, organo) 
+							VALUES (:id_caja, :estado_solenoide, :organo)`, Sede)
+
+    log.Printf("Error creating SmartCase: %v", err)
+    return err
+    
+}
