1fd9adbf2cef4855632bd6c9405adb380c789f36
SilvaFresc <silver.bin.00011000@gmail.com>
2026-05-14 16:59:17 -0500

    Update viajeRepository.go

--- backend/internal/repository/viajeRepository.go
+++ backend/internal/repository/viajeRepository.go
@@ -1,1 +1,34 @@
+package repository
 
+import (
+
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	"backend/internal/models"
+)
+
+
+type ViajeRepository struct{
+	db *sqlx.DB
+}
+
+func NewViajeCaseRepository(db *sqlx.DB) *ViajeRepository{
+
+	if db == nil {
+		panic("No puedes crear un viaje sin una base de datos")
+	}
+	return &ViajeRepository{db:db}
+}
+
+func (r *ViajeRepository) CrearViaje(viaje *models.Viaje) error {
+
+	_ ,err := r.db.NamedExec(`INSERT INTO viaje (id_viaje,id_caja,id_usuario_conductor,id_usuario_receptor,id_sede_origen,id_sede_destino,id_ambulancia,
+                                              fecha_inicio,fecha_llegada,estado_viaje) VALUES (:id_viaje,:id_caja,:id_usuario_conductor,:id_usuario_receptor,:id_sede_origen,:id_sede_destino,:id_ambulancia,
+                                              :fecha_inicio, :fecha_llegada,:estado_viaje)`, viaje)
+
+    log.Printf("Error creating viaje: %v", err)
+    return err
+
+  }
+    
